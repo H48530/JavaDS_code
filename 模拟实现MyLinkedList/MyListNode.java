00382c8d9bb8ffeecd01d3403177533bdff63fcf
H48530 <485301267@qq.com>
2021-03-20 22:24:59 +0800

    MyListNode

---
+++ 模拟实现MyLinkedList/MyListNode.java
@@ -0,0 +1,15 @@
+package Day0320;
+
+public class MyListNode {
+    public String val;
+
+    public MyListNode prev;
+
+    public MyListNode next;
+
+    public MyListNode(String val) {
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
