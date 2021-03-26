f34d3920887e0e1b13271956ea8371b38b0ad8d0
H48530 <485301267@qq.com>
2021-03-26 19:27:16 +0800

    结点的定义

---
+++ 二叉树的基本操作实现/TreeNode.java
@@ -0,0 +1,19 @@
+/**
+ * 二叉树的结点
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("TreeNode{%c}", val);
+    }
+}
