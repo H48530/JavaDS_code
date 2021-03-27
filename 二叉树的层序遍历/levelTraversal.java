dd3ee2166186a7a309076b332c0aee3cd2e39d62
H48530 <485301267@qq.com>
2021-03-27 22:22:11 +0800

    层序遍历的实现

---
+++ 二叉树的层序遍历/levelTraversal.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ */
+public class levelTraversal {
+    public void level(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            System.out.println((char) node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+}
