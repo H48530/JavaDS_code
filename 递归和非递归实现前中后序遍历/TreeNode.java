ab10d82f73a22b90b6d2c7e088e9f12cb763bfc9
H48530 <485301267@qq.com>
2021-03-28 14:00:48 +0800

    结点

---
+++ 递归和非递归实现前中后序遍历/TreeNode.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%c", val);
+    }
+}
