13093c38f9cf7bbf88f82455bc39c217551876d5
H48530 <485301267@qq.com>
2021-04-06 20:56:06 +0800

    交换数组中两个元素

---
+++ 七大排序/Swap.java
@@ -0,0 +1,10 @@
+/**
+ *
+ */
+public class Swap {
+    public static void swap(long[] array, int i, int j) {
+        long t = array[i];
+        array[i] = array[j];
+        array[j] = t;
+    }
+}
