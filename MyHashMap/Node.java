137ffcfb8513b204eaea030a43558a3dc5369b50
H48530 <485301267@qq.com>
2021-04-15 23:11:53 +0800

    Node

---
+++ MyHashMap/Node.java
@@ -0,0 +1,16 @@
+package MyHashMap;
+
+/**
+ *
+ */
+public class Node {
+    public String key;
+
+    public Integer value;
+    public Node next;
+
+    public Node(String key, Integer value) {
+        this.key = key;
+        this.value = value;
+    }
+}
